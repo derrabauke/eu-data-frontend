f13fdb0826736b1c4b9e3ba5d2915495db3fb631
Falk <falk.neumann@adfinis.com>
2022-08-20 15:56:39 +0200

    chore: adapt rootURL since relative url is not working

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = './';
+    ENV.rootURL = 'https://derrabauke.github.io/eu-data-frontend/';
   }
 
   return ENV;
